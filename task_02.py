8656d2b5f33e8ac9be6d05c6b991f12f0d034d0a
javedkhan-IS210 <javed.khan@spsmail.cuny.edu>
2016-09-08 22:39:38 +0000

    corrected task 2 and 3

--- task_02.py
+++ task_02.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Prints hello student."""
 
-print """Hello Student!"""
+print "Hello Student!"
--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Different values."""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
