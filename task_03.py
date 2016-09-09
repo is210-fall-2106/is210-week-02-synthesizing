e23dbb2cba72156c412b896c59fd7dad29d7ebc9
Sunil Prajapati <nrk_619@yahoo.com>
2016-09-09 03:57:47 +0000

    error removed

--- task_03.py
+++ task_03.py
@@ -1,7 +1,7 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" These are the names"""
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
